255861ec895811248d32126d0458a31f2289f8cb
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-07-23 17:25:01 +0530

    Encapsulation for infinite queue to handle the instance correctly

--- src/infinite-memory-queue.js
+++ src/infinite-memory-queue.js
@@ -1,44 +1,49 @@
-let createNode = function createNode(data) {
-        return {
-            data: data,
-            next: undefined
+export default function createQueue() {
+    let createNode = function createNode(data) {
+            return {
+                data: data,
+                next: undefined
+            };
+        },
+        entryNode = undefined,
+        exitNode = undefined,
+        enqueue = function enqueue(data) {
+            let newNode = createNode(data);
+            if (exitNode === undefined)
+                exitNode = newNode;
+
+            newNode.next = entryNode;
+            entryNode = newNode;
+        },
+        dequeue = function dequeue() {
+            let returnData = exitNode === undefined ? undefined : exitNode.data;
+
+            ( function reinstateExitNode(node) {
+                if (node === undefined)
+                    return;
+
+                let isLastNode = node.next === undefined && node === exitNode,
+                    isNonLastNode = node.next === exitNode;
+
+                if (isLastNode)
+                    entryNode = exitNode = undefined;
+
+                if (isNonLastNode) {
+                    exitNode = node;
+                    exitNode.next = undefined;
+                }
+
+                reinstateExitNode(node.next);
+            } )(entryNode);
+            return returnData;
+        },
+        peek = function peek() {
+            return entryNode !== undefined ? entryNode.data : undefined;
         };
-    },
-    entryNode = undefined,
-    exitNode = undefined,
-    enqueue = function enqueue(data) {
-        let newNode = createNode(data);
-        if (exitNode === undefined)
-            exitNode = newNode;
 
-        newNode.next = entryNode;
-        entryNode = newNode;
-    },
-    dequeue = function dequeue() {
-        let returnData = exitNode === undefined ? undefined : exitNode.data;
-
-        ( function reinstateExitNode(node) {
-            if (node === undefined)
-                return;
-
-            let isLastNode = node.next === undefined && node === exitNode,
-                isNonLastNode = node.next === exitNode;
-
-            if (isLastNode)
-                entryNode = exitNode = undefined;
-
-            if (isNonLastNode) {
-                exitNode = node;
-                exitNode.next = undefined;
-            }
-
-            reinstateExitNode(node.next);
-        } )(entryNode);
-
-        return returnData;
-    },
-    peek = function peek() {
-        return entryNode !== undefined ? entryNode.data : undefined;
+    return {
+        enqueue: enqueue,
+        dequeue: dequeue,
+        peek: peek
     };
-
-export { enqueue, dequeue, peek };
+}
--- test/infinite-memory-queue-test.js
+++ test/infinite-memory-queue-test.js
@@ -1,8 +1,9 @@
 import { assert } from 'chai';
-import * as queue from '../src/infinite-memory-queue';
+import createQueue from '../src/infinite-memory-queue';
 
 describe('Queue operations', () => {
     it('Add items to queue', () => {
+        let queue = createQueue();
         queue.enqueue(19);
         queue.enqueue(20);
         assert.equal(queue.dequeue(), 19);
@@ -10,17 +11,18 @@ describe('Queue operations', () => {
     });
 
     it('Remove items from queue', () => {
+        let queue = createQueue();
         queue.enqueue(19);
         assert.equal(queue.dequeue(), 19);
         assert.equal(queue.dequeue(), undefined);
     });
 
     it('Peek inside queue', () => {
+        let queue = createQueue();
+
         assert.equal(queue.peek(), undefined);
         queue.enqueue(19);
         queue.enqueue(20);
         assert.equal(queue.peek(), 20);
-        queue.dequeue();
-        queue.dequeue();
     });
 });
