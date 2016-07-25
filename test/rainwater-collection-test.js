a818b76726db7542a704c5ea0f3e0f336ff6c108
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-07-25 20:47:07 +0530

    Algorithm for calculating rain-water collected between towers

---
+++ test/rainwater-collection-test.js
@@ -0,0 +1,9 @@
+import { assert } from 'chai';
+import waterCollected from '../src/rainwater-collection';
+
+describe('Rainwater collection problem', () => {
+    it('Collect max water between given towers', () => {
+        assert.equal(waterCollected([1, 2, 5, 1, 1, 7, 4]), 8);
+        assert.equal(waterCollected([1, 5, 2, 3, 1, 7, 2, 4]), 11);
+    });
+});
