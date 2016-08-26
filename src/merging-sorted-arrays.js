890f440ed27be824ad1f66fa41bd8ac714001580
Ramesh <yrameshrao@gmail.com>
2016-08-26 06:59:21 +0530

    Merging sorted array inside an array with buffer

---
+++ src/merging-sorted-arrays.js
@@ -0,0 +1,23 @@
+export default function mergeSortedArrays(first, second) {
+    let lastIndexOfFirstArray = first.findIndex((elem) => {
+            return elem === undefined;
+        }) - 1,
+        lastIndexOfSecondArray = second.length - 1,
+        totalLength = first.length - 1;
+
+    while (totalLength > 0) {
+        let elemA = first[lastIndexOfFirstArray],
+            elemB = second[lastIndexOfSecondArray];
+        if (elemA > elemB) {
+            first[totalLength] = elemA;
+            lastIndexOfFirstArray--;
+        } else {
+            first[totalLength] = elemB;
+            lastIndexOfSecondArray--;
+        }
+
+        totalLength--;
+    }
+
+    return first;
+}
---
+++ test/merging-sorted-arrays-test.js
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+import mergeSortedArrays from '../src/merging-sorted-arrays';
+
+describe('Merge sorted arrays', () => {
+    it('Folding one array into another with sorted order', () => {
+        let first = [1, 3, 5, 6, 9, 10, undefined, undefined, undefined, undefined],
+            second = [2, 4, 7, 8];
+
+        mergeSortedArrays(first, second);
+
+        assert.deepEqual(first, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+});
