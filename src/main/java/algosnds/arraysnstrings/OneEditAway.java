845776efbaaa071b4435e4aad8a6235ea6b8ac5f
Ramesh <yrameshrao@gmail.com>
2016-09-21 05:40:17 +0530

    Algo for verifying if to strings are one modification away only

---
+++ src/main/java/algosnds/arraysnstrings/OneEditAway.java
@@ -0,0 +1,52 @@
+package algosnds.arraysnstrings;
+
+public class OneEditAway{
+
+	public boolean verify(String first, String second){
+
+		if(first.length()==second.length())
+			return verifyReplace(first, second);
+
+		if(first.length() + 1 == second.length())
+			return verifyAdditionOrRemoval(first, second);
+
+		if(first.length() - 1 == second.length())
+			return verifyAdditionOrRemoval(second, first);
+	
+		return false;
+	}
+
+	private boolean verifyAdditionOrRemoval(String s1, String s2){
+		int index1 = 0, index2 = 0;
+		while(index1<s1.length() && index2<s2.length()){
+			if(s1.charAt(index1)!=s2.charAt(index2)){
+				if(index1!=index2)
+					return false;
+
+				index2++;
+			}
+			else{
+				index1++;
+				index2++;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean verifyReplace(String s1, String s2){
+		int index = 0;
+		boolean replaceFound = false;
+		while(index<s1.length()){
+			if(s1.charAt(index)!=s2.charAt(index)){
+				if(replaceFound)
+					return false;
+
+				replaceFound = true;
+			}
+			index++;
+		}
+
+		return true;
+	}
+}
---
+++ src/test/java/algosnds/arraysnstrings/OneEditAwayTest.java
@@ -0,0 +1,21 @@
+package algosnds.arraysnstrings;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class OneEditAwayTest{
+
+	OneEditAway oneEditAway = new OneEditAway();
+
+	@Test
+	public void verfiy_input_string_output_string_are_one_edit_away(){
+		assertThat(oneEditAway.verify("pale", "ple"), is(true));
+		assertThat(oneEditAway.verify("ple", "pale"), is(true));
+		assertThat(oneEditAway.verify("pale", "pabe"), is(true));
+		assertThat(oneEditAway.verify("pale", "pabl"), is(false));
+		assertThat(oneEditAway.verify("pale", "pe"), is(false));
+		assertThat(oneEditAway.verify("ple", "pafe"), is(false));
+	}
+}
