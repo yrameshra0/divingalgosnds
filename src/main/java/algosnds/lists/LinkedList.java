ad8e5a2b09cb1462641972c6eaae4786aeb64dd4
Ramesh <yrameshrao@gmail.com>
2016-10-04 05:10:06 +0530

    Equals and hashcode methods for classes

--- src/main/java/algosnds/lists/LinkedList.java
+++ src/main/java/algosnds/lists/LinkedList.java
@@ -1,5 +1,7 @@
 package algosnds.lists;
 
+import java.util.Objects;
+
 import static algosnds.lists.Node.newNode;
 
 public class LinkedList<T> {
@@ -15,5 +17,33 @@ public class LinkedList<T> {
             }
             temp.next = newNode(data);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinkedList<?> that = (LinkedList<?>) o;
+        return Objects.equals(root, that.root);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(root);
+    }
+
+    @Override
+    public String toString() {
+        Node<T> temp = root;
+        StringBuilder builder = new StringBuilder("LinkedList : ");
+
+        while (temp != null) {
+            builder.append(temp.data);
+            if (temp.next != null)
+                builder.append("->");
+            temp = temp.next;
+        }
+
+        return builder.toString();
     }
 }
--- src/main/java/algosnds/lists/Node.java
+++ src/main/java/algosnds/lists/Node.java
@@ -1,5 +1,7 @@
 package algosnds.lists;
 
+import java.util.Objects;
+
 public class Node<T> {
     public final T data;
     public Node<T> next;
@@ -10,5 +12,19 @@ public class Node<T> {
 
     public static <T> Node<T> newNode(T data) {
         return new Node<>(data);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(data, node.data) &&
+                Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, next);
     }
 }
