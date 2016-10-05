6ce100506e3433ac700f5d5c20dd8f58605b439f
Ramesh <yrameshrao@gmail.com>
2016-10-06 05:27:34 +0530

    Differentiation between forward and reverse addition of the lists
    Supports forward addtions alongside with reverse addition of the lists

--- src/main/java/algosnds/lists/SumLists.java
+++ src/main/java/algosnds/lists/SumLists.java
@@ -2,17 +2,17 @@ package algosnds.lists;
 
 public class SumLists {
 
-    public LinkedList<Integer> add(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+    public LinkedList<Integer> addReverseList(LinkedList<Integer> list1, LinkedList<Integer> list2) {
         Node<Integer> first = list1.root;
         Node<Integer> second = list2.root;
 
         LinkedList<Integer> sumList = new LinkedList<>();
 
-        recursiveAdd(first, second, 0, sumList);
+        reverseRecursiveAdd(first, second, 0, sumList);
         return sumList;
     }
 
-    private void recursiveAdd(Node<Integer> first, Node<Integer> second, Integer carry, LinkedList<Integer> accum) {
+    private void reverseRecursiveAdd(Node<Integer> first, Node<Integer> second, Integer carry, LinkedList<Integer> accum) {
         if (first == null && second == null && carry == 0)
             return;
 
@@ -36,6 +36,65 @@ public class SumLists {
 
         accum.addToTail(sum);
 
-        recursiveAdd(first, second, carry, accum);
+        reverseRecursiveAdd(first, second, carry, accum);
+    }
+
+    public LinkedList<Integer> addForwardList(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        addPadToLists(list1, list2);
+
+        Node<Integer> first = list1.root;
+        Node<Integer> second = list2.root;
+
+        CarryAndListHolder holder = reverseForwardAdd(first, second);
+        
+        // Pushing the carry  over the last element on the top of the list
+        if (holder.carry > 0)
+            holder.accum.addToHead(holder.carry);
+
+        return holder.accum;
+    }
+
+    private void addPadToLists(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        int firstListLength = list1.length();
+        int secondListLength = list2.length();
+
+        if (firstListLength > secondListLength)
+            pad(list2, (firstListLength - secondListLength));
+        else if (secondListLength > firstListLength)
+            pad(list1, (secondListLength - firstListLength));
+    }
+
+    private void pad(LinkedList<Integer> list, int length) {
+        for (int i = 0; i < length; i++)
+            list.addToHead(0);
+    }
+
+    private CarryAndListHolder reverseForwardAdd(Node<Integer> first, Node<Integer> second) {
+        if (first == null && second == null)
+            return new CarryAndListHolder();
+
+        CarryAndListHolder holder = reverseForwardAdd(first.next, second.next);
+
+        int sum = first.data + second.data + holder.carry;
+        holder.carry = 0;
+
+        if (sum >= 10) {
+            holder.carry = 1;
+            sum -= 10;
+        }
+
+        holder.accum.addToHead(sum);
+
+        return holder;
+    }
+
+    private class CarryAndListHolder {
+        private int carry;
+        private LinkedList<Integer> accum;
+
+        private CarryAndListHolder() {
+            this.carry = 0;
+            this.accum = new LinkedList<>();
+        }
     }
 }
--- src/test/java/algosnds/lists/SumListsTest.java
+++ src/test/java/algosnds/lists/SumListsTest.java
@@ -10,29 +10,36 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class SumListsTest {
 
     @Test
-    public void add_two_equal_sized_linked_lists() {
+    public void add_two_equal_sized_reversed_linked_lists() {
         SumLists sumlists = new SumLists();
 
-        LinkedList<Integer> summedList = sumlists.add(createList(7, 1, 6), createList(5, 9, 2));
+        LinkedList<Integer> summedList = sumlists.addReverseList(createList(7, 1, 6), createList(5, 9, 2));
 
         assertThat(summedList, is(createList(2, 1, 9)));
     }
 
     @Test
-    public void add_two_unequal_sized_linked_lists() {
+    public void add_two_unequal_sized_reversed_linked_lists() {
         SumLists sumlists = new SumLists();
 
-        assertThat(sumlists.add(createList(4, 3, 9, 1), createList(4, 3, 9)), is(createList(8, 6, 8, 2)));
-        assertThat(sumlists.add(createList(4, 3, 9), createList(4, 3, 9, 1)), is(createList(8, 6, 8, 2)));
+        assertThat(sumlists.addReverseList(createList(4, 3, 9, 1), createList(4, 3, 9)), is(createList(8, 6, 8, 2)));
+        assertThat(sumlists.addReverseList(createList(4, 3, 9), createList(4, 3, 9, 1)), is(createList(8, 6, 8, 2)));
     }
 
     @Test
-    public void add_two_equal_sized_linked_lists_pending_carry() {
+    public void add_two_equal_sized_reversed_linked_lists_pending_carry() {
         SumLists sumlists = new SumLists();
 
-        assertThat(sumlists.add(createList(4, 3, 9), createList(4, 3, 9)), is(createList(8, 6, 8, 1)));
+        assertThat(sumlists.addReverseList(createList(4, 3, 9), createList(4, 3, 9)), is(createList(8, 6, 8, 1)));
     }
 
+    @Test
+    public void add_two_forward_linked_lists() {
+        SumLists sumlists = new SumLists();
+
+        assertThat(sumlists.addForwardList(createList(6, 1, 7), createList(9, 5)), is(createList(7, 1, 2)));
+        assertThat(sumlists.addForwardList(createList(9, 3, 4), createList(9, 3, 4)), is(createList(1, 8, 6, 8)));
+    }
 
     private LinkedList<Integer> createList(Integer... elements) {
         LinkedList<Integer> finalList = new LinkedList<>();
