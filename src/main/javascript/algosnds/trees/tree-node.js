6142b4e9e42e16513488f35c8cbe8e0c7e70ebf6
Ramesh <yrameshrao@gmail.com>
2016-11-25 16:53:52 +0530

    Parent added to normal tree node and subsequent test modifications

--- src/main/javascript/algosnds/trees/tree-node.js
+++ src/main/javascript/algosnds/trees/tree-node.js
@@ -2,17 +2,9 @@ function createTreeNode(data) {
     return {
         data: data,
         left: undefined,
-        right: undefined
-    };
-}
-
-function createTreeNodeWithParent(data, parent) {
-    return {
-        data: data,
-        left: undefined,
         right: undefined,
-        parent: parent
+        parent: undefined
     };
 }
 
-export { createTreeNode, createTreeNodeWithParent };
+export { createTreeNode };
--- src/test/javascript/algosnds/trees/tree-node-test.js
+++ src/test/javascript/algosnds/trees/tree-node-test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { createTreeNode, createTreeNodeWithParent } from '../../../../main/javascript/algosnds/trees/tree-node';
+import { createTreeNode } from '../../../../main/javascript/algosnds/trees/tree-node';
 
 describe('Generate tree', () => {
     it('Create tree node for formulating entire tree', () => {
@@ -12,24 +12,29 @@ describe('Generate tree', () => {
             left: {
                 data: 1,
                 left: undefined,
-                right: undefined
+                right: undefined,
+                parent: undefined
             },
             right: {
                 data: 10,
                 left: undefined,
-                right: undefined
-            }
+                right: undefined,
+                parent: undefined
+            },
+            parent: undefined
         };
 
         assert.deepEqual(tree, expectedTree);
     });
 
-    it('Create tree node with paren for formulating entire tree', () => {
-        let parent = createTreeNodeWithParent(5, undefined),
-            tree = Object.assign({}, parent);
+    it('Create tree node with parent setting while formulating entire tree', () => {
+        let tree = createTreeNode(5);
 
-        tree.left = createTreeNodeWithParent(1, parent);
-        tree.right = createTreeNodeWithParent(10, parent);
+        tree.left = createTreeNode(1);
+        tree.left.parent = tree;
+
+        tree.right = createTreeNode(10);
+        tree.right.parent = tree;
 
         let expectedTree = {
             data: 5,
@@ -37,26 +42,19 @@ describe('Generate tree', () => {
                 data: 1,
                 left: undefined,
                 right: undefined,
-                parent: {
-                    data: 5,
-                    left: undefined,
-                    right: undefined,
-                    parent: undefined
-                }
+                parent: undefined,
             },
             right: {
                 data: 10,
                 left: undefined,
                 right: undefined,
-                parent: {
-                    data: 5,
-                    left: undefined,
-                    right: undefined,
-                    parent: undefined
-                }
+                parent: undefined,
             },
             parent: undefined
         };
+
+        expectedTree.left.parent = expectedTree;
+        expectedTree.right.parent = expectedTree;
 
         assert.deepEqual(tree, expectedTree);
     });
