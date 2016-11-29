bad6d0ba7f0b6226d1f1739f4e6fb5741402dc39
Ramesh <yrameshrao@gmail.com>
2016-11-30 05:17:46 +0530

    Improving algorithm with catching of finding lowest common ancestor
    Catching situations when no tree node is existent is sent it for finding
    the common ancestor and improving code around that problem.

--- src/main/javascript/algosnds/trees/lowest-common-ancestor-b-tree.js
+++ src/main/javascript/algosnds/trees/lowest-common-ancestor-b-tree.js
@@ -1,19 +1,37 @@
 export default function lowestCommonAncestor(curr, nodeA, nodeB) {
+    let newResult = function newResult(node, ancestor) {
+            return {
+                node: node,
+                isAncestor: ancestor
+            };
+        },
+        result = ( function commonAncestorHelper(curr, nodeA, nodeB) {
+            if (curr === undefined)
+                return newResult(undefined, false);
 
-    if (curr === undefined)
+            if (curr === nodeA && curr === nodeB)
+                return newResult(curr, true);
+
+            let left = commonAncestorHelper(curr.left, nodeA, nodeB);
+            if (left.isAncestor)
+                return left;
+
+            let right = commonAncestorHelper(curr.right, nodeA, nodeB);
+            if (right.isAncestor)
+                return right;
+
+            if (left.node !== undefined && right.node !== undefined) {
+                return newResult(curr, true);
+            } else if (curr === nodeA || curr === nodeB) {
+                return newResult(curr, left.node !== undefined || right.node !== undefined);
+            }
+            else
+                return newResult(left.node !== undefined ? left.node : right.node, false);
+
+        } )(curr, nodeA, nodeB);
+
+    if (!result.isAncestor)
         return undefined;
 
-    if (curr === nodeA || curr === nodeB)
-        return curr;
-
-    let left = lowestCommonAncestor(curr.left, nodeA, nodeB);
-    let right = lowestCommonAncestor(curr.right, nodeA, nodeB);
-
-    if (left !== undefined && right !== undefined)
-        return curr;
-
-    if (left === undefined)
-        return right;
-    else
-        return left;
+    return result.node;
 }
--- src/test/javascript/algosnds/trees/lowest-common-ancestor-b-tree-test.js
+++ src/test/javascript/algosnds/trees/lowest-common-ancestor-b-tree-test.js
@@ -2,21 +2,22 @@ import { assert } from 'chai';
 import lowestCommonAncestor from '../../../../main/javascript/algosnds/trees/lowest-common-ancestor-b-tree';
 import { createTreeNode } from '../../../../main/javascript/algosnds/trees/tree-node';
 
-describe('Lowest common ancestor in Binary Tree', () => {
-    it('Gives lowest common ancestor between 2 nodes of tree', () => {
-        let tree = createTreeNode(1);
-        tree.left = createTreeNode(2);
-        tree.left.left = createTreeNode(4);
-        tree.left.right = createTreeNode(5);
-        tree.left.right.left = createTreeNode(7);
-        tree.left.right.right = createTreeNode(8);
-        tree.right = createTreeNode(3);
+describe('Lowest common ancestor between two nodes in Binary Tree', () => {
+    let tree = createTreeNode(1);
+    tree.left = createTreeNode(2);
+    tree.left.left = createTreeNode(4);
+    tree.left.right = createTreeNode(5);
+    tree.left.right.left = createTreeNode(7);
+    tree.left.right.right = createTreeNode(8);
+    tree.right = createTreeNode(3);
 
-        let node4 = tree.left.left,
-            node8 = tree.left.right.right,
-            node2 = tree.left;
+    let node4 = tree.left.left,
+        node8 = tree.left.right.right,
+        node2 = tree.left,
+        randomNode = createTreeNode(100);
 
-        let lowestCommonAncestorNode = lowestCommonAncestor(tree, node4, node8)
-        assert.deepEqual(lowestCommonAncestorNode, node2);
+    it('Gives lowest common ancestor using no parent traversal', () => {
+        assert.deepEqual(lowestCommonAncestor(tree, node4, node8), node2);
+        assert.isUndefined(lowestCommonAncestor(tree, node4, randomNode));
     });
 });
