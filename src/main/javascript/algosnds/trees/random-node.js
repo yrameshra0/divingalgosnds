79184a443ec6711f48febfb032dcf27cf377b49f
Ramesh <yrameshrao@gmail.com>
2016-12-14 05:13:19 +0530

    Basic handling of the tree formation modified
    Along-side making sure the behaviour remains same on the outside of the
    function
    
    Deletion of tree node implemented, WIP for non-leaf nodes

--- src/main/javascript/algosnds/trees/random-node.js
+++ src/main/javascript/algosnds/trees/random-node.js
@@ -1,55 +1,83 @@
-const treeNode = function treeNode(newData) {
-    const data = newData;
-    let size = 0;
-    let left = undefined;
-    let right = undefined;
-    const insertInOrder = function insertInOrder(newData) {
-        if (newData <= data) {
-            if (left === undefined) {
-                left = treeNode(newData);
-            } else {
-                left.insertInOrder(newData);
-            }
-        } else {
-            if (right === undefined) {
-                right = treeNode(newData);
-            } else {
-                right.insertInOrder(newData);
-            }
-        }
-        this.size++;
-    };
+export default function newTree() {
 
-    const find = function find(data) {
-        if (data === this.data)
-            return this;
+    let createTreeNode = function createTreeNode(data) {
+            return {
+                data: data,
+                left: undefined,
+                right: undefined,
+                parent: undefined,
+                size: 0
+            };
+        },
+        root = undefined,
+        insertInOrder = function insertInOrder(data) {
+            if (root === undefined) {
+                root = createTreeNode(data);
+                ++root.size;
+            } else {
+                insertInOrderRecur(root, data);
+            }
+        },
+        insertInOrderRecur = function insertInOrder(node, data) {
+            if (data < node.data) {
+                if (node.left === undefined) {
+                    node.left = createTreeNode(data);
+                    node.left.parent = node;
+                } else {
+                    insertInOrderRecur(node.left, data);
+                }
+            } else {
+                if (node.right === undefined) {
+                    node.right = createTreeNode(data);
+                    node.right.parent = node;
+                } else {
+                    insertInOrderRecur(node.right, data);
+                }
+            }
+            ++node.size;
+        },
+        find = function find(data) {
+            return findRecur(root, data);
+        },
+        findRecur = function findRecur(node, data) {
+            if (node === undefined)
+                return;
 
-        if (data < this.data) {
-            return left !== undefined ? left.find(data) : undefined;
-        } else {
-            return right !== undefined ? right.find(data) : undefined;
-        }
-    };
+            if (node.data === data)
+                return node;
+
+            if (data < node.data) {
+                return findRecur(node.left, data);
+            } else {
+                return findRecur(node.right, data);
+            }
+        },
+        deleteNode = function deleteNode(data) {
+            let nodeToDelete = find(data);
+            if (nodeToDelete.left === undefined && nodeToDelete.right === undefined) {
+                ( function deleteRootNode(nodeToDelete) {
+                    if (nodeToDelete.parent.left === nodeToDelete) {
+                        nodeToDelete.parent.left = undefined;
+                    } else {
+                        nodeToDelete.parent.right = undefined;
+                    }
+                } )(nodeToDelete);
+            }
+
+            ( function updateTreeSizes(nodeToDelete) {
+                while (nodeToDelete.parent !== undefined) {
+                    nodeToDelete.parent.size = nodeToDelete.parent.size - 1;
+                    nodeToDelete = nodeToDelete.parent;
+
+                }
+            } )(nodeToDelete);
+
+            console.log(root);
+        };
 
     return {
-        data: data,
-        left: left,
-        right: right,
-        size: size,
         insertInOrder: insertInOrder,
-        find: find
+        find: find,
+        deleteNode: deleteNode
     };
 }
-let root = undefined;
-const insertInOrder = function insertInOrder(newData) {
-    if (root === undefined) {
-        root = treeNode(newData);
-    } else {
-        root.insertInOrder(newData);
-    }
-};
-const find = function find(data) {
-    return root.find(data);
-};
-
-export { insertInOrder, find };
--- src/test/javascript/algosnds/trees/random-node-test.js
+++ src/test/javascript/algosnds/trees/random-node-test.js
@@ -1,8 +1,9 @@
 import { assert } from 'chai';
-import * as tree from '../../../../main/javascript/algosnds/trees/random-node';
+import newTree from '../../../../main/javascript/algosnds/trees/random-node';
 
 describe('Tree implementation than can retrieve an any random node with equal probability', () => {
     it('Insert node into tree', () => {
+        const tree = newTree();
         tree.insertInOrder(20);
         assert.equal(tree.find(20).data, 20);
 
@@ -13,6 +14,35 @@ describe('Tree implementation than can retrieve an any random node with equal pr
         assert.isUndefined(tree.find(100));
     });
     it('Find node from tree');
-    it('Delete node from tree');
+    it('Delete node from tree', () => {
+        const tree = newTree();
+
+        tree.insertInOrder(20);
+        tree.insertInOrder(15);
+        tree.insertInOrder(25);
+
+        tree.insertInOrder(9);
+        tree.insertInOrder(17);
+        tree.insertInOrder(23);
+        tree.insertInOrder(28);
+
+        tree.insertInOrder(8);
+        tree.insertInOrder(10);
+        tree.insertInOrder(27);
+        tree.insertInOrder(30);
+
+        tree.insertInOrder(6);
+        tree.insertInOrder(7);
+        tree.insertInOrder(26);
+
+        assertDeletion(tree, 7);
+        assertDeletion(tree, 6);
+    });
+
+    function assertDeletion(tree, nodeData) {
+        assert.isDefined(tree.find(nodeData));
+        tree.deleteNode(nodeData);
+        assert.isUndefined(tree.find(nodeData));
+    }
     it('Fetch random node from tree');
 });
