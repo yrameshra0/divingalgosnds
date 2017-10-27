233786d23f8d58588149543096fea61d3f59fffa
Y Ramesh Rao <yrameshrao@gmail.com>
2017-10-27 10:51:12 +0530

    Tower of hanoi of implementation

---
+++ src/main/java/algosnds/general/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+package algosnds.general;
+
+import algosnds.stacks.Stack;
+
+public class TowerOfHanoi {
+
+    public static void shiftFromSourceToDestination(Stack<Integer> source, Stack<Integer> destination, Stack<Integer> spare) {
+        shifting(source.length(), source, destination, spare);
+    }
+
+    private static void shifting(int n, Stack<Integer> source, Stack<Integer> destination, Stack<Integer> spare) {
+        if(n<=0) return;
+
+        shifting(n-1, source, spare, destination);
+        destination.push(source.pop());
+        shifting(n-1, spare, destination, source);
+    }
+}
---
+++ src/test/java/algosnds/general/TowerOfHanoiTest.java
@@ -0,0 +1,33 @@
+package algosnds.general;
+
+import algosnds.stacks.Stack;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+public class TowerOfHanoiTest {
+
+    @Test
+    public void play_tower_of_hanoi() throws Exception {
+        Stack<Integer> sourceTower = new Stack<>();
+        sourceTower.push(5);
+        sourceTower.push(4);
+        sourceTower.push(3);
+        sourceTower.push(2);
+        sourceTower.push(1);
+
+        Stack<Integer> spareTower = new Stack<>();
+        Stack<Integer> destinationTower = new Stack<>();
+
+        TowerOfHanoi.shiftFromSourceToDestination(sourceTower, destinationTower, spareTower);
+
+        assertThat(sourceTower.isEmpty(), is(true));
+        assertThat(spareTower.isEmpty(), is(true));
+
+        assertThat(destinationTower.pop(), is(1));
+        assertThat(destinationTower.pop(), is(2));
+        assertThat(destinationTower.pop(), is(3));
+        assertThat(destinationTower.pop(), is(4));
+        assertThat(destinationTower.pop(), is(5));
+    }
+}
