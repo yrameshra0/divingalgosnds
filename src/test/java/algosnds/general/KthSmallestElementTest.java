b2fd636e10e5fc820908cfe3dc302258a6ee814c
Y Ramesh Rao <yrameshrao@gmail.com>
2017-11-21 11:30:24 +0530

    Randomized kTh Element search and heuristics reporting

--- src/test/java/algosnds/general/KthSmallestElementTest.java
+++ src/test/java/algosnds/general/KthSmallestElementTest.java
@@ -1,19 +1,34 @@
 package algosnds.general;
 
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import static algosnds.general.KthSmallestElement.findKthElement;
+import static algosnds.general.KthSmallestElement.findKthElementDeterministic;
+import static algosnds.general.KthSmallestElement.findKthElementRandom;
+import static java.util.stream.LongStream.range;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class KthSmallestElementTest {
+    public static final Integer MAX_ITERATIONS_NO = 1_000_000;
 
-    @Test
-    public void deterministic_pivot_element_search() {
+    private static Integer kThElement = 7;
+    private static Integer expectedValue = 124;
+    private static Runnable deterministicAssertion = () -> {
+        assertThat(expectedValue, is(findKthElementDeterministic(elementsList(), kThElement)));
+    };
 
+    private static Runnable randomizedAssertion = () -> {
+        assertThat(expectedValue, is(findKthElementRandom(elementsList(), kThElement)));
+    };
+
+    private static List<Integer> elementsList() {
         Integer[] elementArray = {
                 800, 100, 200, 124, 160,
                 105, 1, 69, 34, 60,
@@ -21,11 +36,67 @@ public class KthSmallestElementTest {
                 821, 201, 701, 502, 601,
                 239, 603, 235, 710, 653
         };
-        int k = 7;
 
-        Integer expectedValue = 124;
+        List<Integer> elements = new ArrayList<>(10_000);
+        elements.addAll(Arrays.asList(elementArray));
+        Random random = new Random();
 
-        assertThat(expectedValue, is(findKthElement(Arrays.asList(elementArray), k)));
+        range(25, 10_000).forEach(idx -> {
+            elements.add(900 + random.nextInt(9_000));
+        });
+
+        return elements;
+    }
+
+    @Test
+    public void using_deterministic_median_algorithm_internally() {
+        deterministicAssertion.run();
+    }
+
+    @Test
+    public void using_random_median_algorithm_internally() {
+        randomizedAssertion.run();
+    }
+
+    @Test
+    @Ignore
+    // Comparison test only
+    public void heuristic_comparisons_between_internal_algos() {
+        BiConsumer<TreeSet<Long>, String> printHeuristics = (set, type) -> {
+            System.out.println("============ ALGORITHM TYPE ============");
+            System.out.println(type.toUpperCase());
+            System.out.println("=========== TIME TAKEN (nanos) =============");
+            System.out.println(String.format("MINIMUM --> %d", set.first()));
+            System.out.println(String.format("MAXIMUM --> %d", set.last()));
+
+            Long reduceRandom = set.stream()
+                    .reduce(0L,
+                            (value, incoming) -> value + incoming);
+            System.out.println("AVERAGE --> " + (reduceRandom / MAX_ITERATIONS_NO));
+            System.out.println("========================");
+        };
+
+        Function<Runnable, TreeSet<Long>> iterateAndCollect = (assertion) -> {
+            return range(0, MAX_ITERATIONS_NO)
+                    .boxed()
+                    .parallel()
+                    .map(idx -> {
+                        long startTime = System.nanoTime();
+                        assertion.run();
+                        return System.nanoTime() - startTime;
+                    })
+                    .collect(Collectors.toCollection(TreeSet::new));
+        };
+
+        range(0, 1)
+                .forEach(odx -> {
+                    TreeSet<Long> deterministicTimes = iterateAndCollect.apply(deterministicAssertion);
+                    TreeSet<Long> randomizedTimes = iterateAndCollect.apply(randomizedAssertion);
+
+                    System.out.println("<--- | " + odx + " | --->");
+                    printHeuristics.accept(deterministicTimes, "deterministic");
+                    printHeuristics.accept(randomizedTimes, "random");
+                });
     }
 
 }
